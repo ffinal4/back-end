e5acbbdc6f2ce04e01d7d487b2667a8b29e66cca
stoow1 <spacegle12341@gmail.com>
2023-07-31 00:09:21 -0400

    feat:첫번째

---
+++ src/main/java/com/example/peeppo/PeeppoApplication.java
@@ -0,0 +1,13 @@
+package com.example.peeppo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeeppoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeeppoApplication.class, args);
+    }
+
+}
