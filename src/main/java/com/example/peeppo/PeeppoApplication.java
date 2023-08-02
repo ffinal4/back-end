af7688e0671fdd2b1f08ac4525b44e228f433bb5
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-08-02 10:22:38 +0900

    Feat: 수정 및 생성시간 구현

--- src/main/java/com/example/peeppo/PeeppoApplication.java
+++ src/main/java/com/example/peeppo/PeeppoApplication.java
@@ -1,9 +1,11 @@
-package com.example.peeppo.utils;
+package com.example.peeppo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PeeppoApplication {
 
     public static void main(String[] args) {
