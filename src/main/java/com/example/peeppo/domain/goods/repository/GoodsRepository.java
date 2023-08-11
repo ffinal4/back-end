7516ab0bed79ddbd359268efa6118ad118bfdd32
jiooong <lucy991010@gmail.com>
2023-08-12 04:13:25 +0900

    feat: 최근 조회한 물품 기능 추가

--- src/main/java/com/example/peeppo/domain/goods/repository/GoodsRepository.java
+++ src/main/java/com/example/peeppo/domain/goods/repository/GoodsRepository.java
@@ -7,13 +7,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface GoodsRepository extends JpaRepository<Goods, Long> {
     Page<Goods> findAllByIsDeletedFalse(Pageable pageable);
     Page<Goods> findAllByUserAndIsDeletedFalse(User user, Pageable pageable);
 
-   // Page<Goods> findGoodsByUser(@Param("userId") Long userId);
+    Goods findDistinctByTitle(String title);
+
+    // Page<Goods> findGoodsByUser(@Param("userId") Long userId);
 
 
 //    List<Goods> findAllByLocationIdAndIsDeletedFalseOrderByGoodsIdDesc(Long locationId);
