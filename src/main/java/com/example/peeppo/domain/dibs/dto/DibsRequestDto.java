34f78be63992155f480483ea3cf5d1184b874701
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-08-15 14:28:52 +0900

    Update: 파일수정

---
+++ src/main/java/com/example/peeppo/domain/dibs/dto/DibsRequestDto.java
@@ -0,0 +1,13 @@
+//package com.example.peeppo.domain.dibs.dto;
+//
+//import jakarta.validation.constraints.NotNull;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@NoArgsConstructor
+//public class DibsRequestDto {
+//
+//    @NotNull
+//    private Long goodsId;
+//}
