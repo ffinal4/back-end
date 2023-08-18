aa880414046254004b6011feb758254f1f3ec50c
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-08-19 04:06:21 +0900

    Feat: errorCode 수정

--- src/main/java/com/example/peeppo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/peeppo/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.peeppo.global.responseDto.ApiResponse;
 import com.example.peeppo.global.responseDto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,45 +14,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullPointerException.class)
-    public ApiResponse<ErrorResponse> handleNullPointerException(Exception ex) {
+    public ResponseEntity handleNullPointerException(Exception ex) {
         log.error("NullPointerException error: {}", ex.getMessage());
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
-        return apiResponse;
+//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<ErrorResponse> handleIllegalArgumentException(Exception ex) {
+    public ResponseEntity handleIllegalArgumentException(Exception ex) {
         log.error("IllegalArgumentException error: {}", ex.getMessage());
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
-        return apiResponse;
+//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(IllegalAccessException.class)
-    public ApiResponse<ErrorResponse> handleIllegalAccessException(Exception ex) {
+    public ResponseEntity handleIllegalAccessException(Exception ex) {
         log.error("IllegalAccessException error: {}", ex.getMessage());
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
-        return apiResponse;
+//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(IllegalStateException.class)
-    public ApiResponse<ErrorResponse> handleIllegalStateException(Exception ex) {
+    public ResponseEntity handleIllegalStateException(Exception ex) {
         log.error("IllegalAccessException error: {}", ex.getMessage());
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
-        return apiResponse;
+//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         StringBuilder sb = new StringBuilder();
         ex.getBindingResult().getFieldErrors().forEach((error) -> {
             sb.append(error.getDefaultMessage()).append(" / ");
         });
         sb.setLength(sb.length() - 3);
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
-        return apiResponse;
+//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//        ApiResponse<ErrorResponse> apiResponse = new ApiResponse<>(false, null, errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
