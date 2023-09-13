e67898a23817e3641f812a7d45638cfd32b027ea
jiooong <lucy991010@gmail.com>
2023-09-14 04:48:07 +0900

    Fix : merge 충돌 오류 해결

--- src/main/java/com/example/peeppo/domain/goods/service/GoodsService.java
+++ src/main/java/com/example/peeppo/domain/goods/service/GoodsService.java
@@ -614,6 +614,7 @@ public class GoodsService {
             requestRepository.save(requestGoods);
         }
         Image image = imageRepository.findByGoodsGoodsIdOrderByCreatedAtAscFirst(sellerGoods.getGoodsId());
+        //Goods onsale로 변경해주기
         notificationService.send(buyer, NotificationStatus.REQUESTREFUSE, sellerGoods.getTitle(), image.getImageUrl());
     }
 
