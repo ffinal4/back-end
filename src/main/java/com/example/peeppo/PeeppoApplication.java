06c9f8a127f38944a0e42cef405d215f1454aa0c
jiooong <lucy991010@gmail.com>
2023-10-23 18:35:02 +0900

    Fix : 오타 수정 및 test, 로깅 코드

--- src/main/java/com/example/peeppo/PeeppoApplication.java
+++ src/main/java/com/example/peeppo/PeeppoApplication.java
@@ -3,6 +3,7 @@ package com.example.peeppo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableCaching
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class PeeppoApplication {
 
     public static void main(String[] args) {
